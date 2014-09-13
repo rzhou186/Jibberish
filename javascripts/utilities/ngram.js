a7313786a81207c38f5dd23b33b8d99915ce9a60
Yushi Wang <mseesquared@gmail.com>
2014-09-13 12:50:02 -0400

    ngrams v1.0 done

--- javascripts/utilities/ngram.js
+++ javascripts/utilities/ngram.js
@@ -13,8 +13,8 @@ var app = app || {};
   };
 
   // Add n-grams for current str
-  var Parse = function(str, curr) {
-    n = this.min + curr;
+  Ngram.prototype.parseString = function(str, curr) {
+    var n = this.min + curr;
 
     // Current str is too short
     if (str.length < n) 
@@ -26,24 +26,26 @@ var app = app || {};
       var key = words.slice(i, i + n).join(" ");
       if (!(key in this.map))
         this.map[key] = [];
-      this.map[key].push(words[i + n + 1]);
+      this.map[key].push(words[i + n]);
     }
-    var key = words.slice(words.length - n, words.length).join(" ");
     // hack TODO: Make more elegant?
+    var key = words.slice(words.length - n, words.length).join(" ");
+    if (!(key in this.map))
+      this.map[key] = [];
     this.map[key].push("STOP");
   }
 
-  Ngram.prototype.parse = function(contents[], min, max) {
-    for (var n = 0; n <= max - min; n++ ) {
+  Ngram.prototype.parseContent = function(contents, min, max) {
+    for (var n = 0; n <= this.max - this.min; n++ ) {
       for (var i = 0; i < contents.length; i++) {
-        this.Parse(contents[i], n);
+        this.parseString(contents[i], n);
       }
     }
     var max = 0;
     for (var key in this.map) {
-      if (map[key].length > max) {
+      if (this.map[key].length > max) {
         this.maxKey = key;
-        max = map[key].length;
+        max = this.map[key].length;
       }
     }
     console.log("Most common key is " + this.maxKey);
@@ -55,11 +57,17 @@ var app = app || {};
     while (string.length < this.maxGen) {
       var candidates = [];
       for (var i = this.min; i <= Math.min(this.max, string.length); i++) {
-        currKey = string.slice(string.length - i, string.length);
+        var currKey = string.slice(string.length - i, string.length);
         candidates = candidates.concat(this.map[currKey.join(" ")]);
       }
+      if (candidates.length < 1)
+        break;
       var rand = Math.floor(Math.random() * candidates.length);
       var next = candidates[rand];
+      if (next === "STOP") {
+        console.log("Hit STOP word.");
+        break;
+      }
       string.push(next);
     }
     return string.join(" ");

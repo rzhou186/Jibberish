7da9d4e8da53f5ba4d0d9116891706b22215e9bd
Ray Zhou <rzhou186@stanford.edu>
2014-09-13 00:59:43 -0400

    Added starting Javascript infrastructure

---
+++ javascripts/utilities/ngram.js
@@ -0,0 +1,9 @@
+var app = app || {};
+
+(function() {
+  "use strict";
+
+  var Ngram = function() {};
+
+  app.ngram = new Ngram();
+})();
---
+++ javascripts/utilities/scraper.js
@@ -0,0 +1,13 @@
+var app = app || {};
+
+(function() {
+  "use strict";
+
+  var Scraper = function() {};
+  
+  Scraper.prototype.scrape = function(url) {
+
+  };
+
+  app.scraper = new Scraper();
+})();

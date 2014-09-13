2a592b238eba3b3758ff06fbe277eb393990494d
Ray Zhou <rzhou186@stanford.edu>
2014-09-13 13:27:05 -0400

    Adding new input components now working

--- javascripts/react/components/source/inputs-component.js
+++ javascripts/react/components/source/inputs-component.js
@@ -4,14 +4,53 @@ var app = app || {};
 (function() {
   "use strict";
 
-  app.InputsComponent = React.createClass({
+  var InputComponent = React.createClass({
+    handleChange: function() {
+      var newInput = this.refs.input.getDOMNode().value;
+      this.props.updateInput(this.props.index, newInput);
+    },
+
     render: function() {
       return (
+        <input type="text" ref="input"
+          defaultValue={this.props.input}
+          onChange={this.handleChange} />
+      );
+    }
+  });
+
+  app.InputsComponent = React.createClass({
+    getInitialState: function() {
+      return { inputs: [""] };
+    },
+
+    addInput: function() {
+      this.setState({
+        inputs: this.state.inputs.concat("")
+      });
+    },
+
+    updateInput: function(index, newInput) {
+      var newInputs = this.state.inputs.concat();
+      newInputs[index] = newInput;
+      this.setState({ inputs: newInputs });
+    },
+
+    render: function() {
+      var inputComponents = [];
+      var that = this;
+      this.state.inputs.forEach(function(input, index) {
+        inputComponents.push(
+          <InputComponent input={input} index={index}
+            updateInput={that.updateInput} />
+        );
+      });
+
+      return (
         <div className="inputs">
-          <input type="text"></input>
-          <input type="text"></input>
-          <button>
-            <span className="glyphicon glyphicon-plus-sign"></span>
+          {inputComponents}
+          <button onClick={this.addInput}>
+            <span className="glyphicon glyphicon-plus-sign" />
             Add Input
           </button>
         </div>
